6e3c8714bc3c822f4954f3564f6ef4f613d6d647
noneals <124405266+noneals@users.noreply.github.com>
2025-11-05 01:19:05 +0300

    Refactor exit handling in builtin.c
    Replace direct exit call with handle_exit function.

--- builtin.c
+++ builtin.c
@@ -38,9 +38,10 @@ int is_builtin(char **args)
     if (args == NULL || args[0] == NULL)
         return (0);
 
-    if (strcmp(args[0], "exit") == 0)
+      if (strcmp(args[0], "exit") == 0)
     {
-        exit(0);
+        handle_exit();
+        return (1);
     }
 
     return (0);
