fa71944f44f94ab82fa1cc30e351d02cf997f984
noneals <124405266+noneals@users.noreply.github.com>
2025-11-04 21:42:37 +0300

    Create find_path.c

---
+++ find_path.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * find_path - Finds command full path
+ */
+char *find_path(char *command)
+{
+    char *path_env, *path_copy, *dir, *full_path;
+    struct stat st;
+
+    if (command[0] == '/' || command[0] == '.')
+    {
+        if (stat(command, &st) == 0)
+            return (strdup(command));
+        return (NULL);
+    }
+
+    path_env = getenv("PATH");
+    if (!path_env)
+        return (NULL);
+
+    path_copy = strdup(path_env);
+    dir = strtok(path_copy, ":");
+
+    while (dir)
+    {
+        full_path = malloc(strlen(dir) + strlen(command) + 2);
+        sprintf(full_path, "%s/%s", dir, command);
+        if (stat(full_path, &st) == 0)
+        {
+            free(path_copy);
+            return (full_path);
+        }
+        free(full_path);
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return (NULL);
+}
