80d9b4bfc83eb446e73d637df3ba9eb7b1561d06
aziz5g-tech <bogico.sa@gmail.com>
2025-11-05 02:16:51 +0300

    fix: solve exit status problems

--- builtin.c
+++ builtin.c
@@ -4,48 +4,50 @@
  * handle_exit - Handles the exit built-in command
  * @line: The input line to free
  * @args: The argument array to free
+ * @status: The last exit status to return
  *
  * Return: void (exits the program)
  */
-void handle_exit(char *line, char **args)
+void handle_exit(char *line, char **args, int status)
 {
     free(line);
     free_array(args);
-	exit(0);
+    exit(status);
 }
 
 /**
-	* handle_env - Handles the env built-in command
-	*
-	* Return: 0 on success
-	*/
+ * handle_env - Handles the env built-in command
+ *
+ * Return: 0 on success
+ */
 int handle_env(void)
 {
-	char **env = environ;
+    char **env = environ;
 
-	while (*env)
-	{
-	printf("%s\n", *env);
-	env++;
-	}
-	return (0);
+    while (*env)
+    {
+        printf("%s\n", *env);
+        env++;
+    }
+    return (0);
 }
 
 /**
  * is_builtin - Checks if command is a built-in command
  * @args: Array of command arguments
  * @line: The input line (for memory cleanup on exit)
+ * @status: The last exit status
  *
  * Return: 1 if built-in executed, 0 otherwise
  */
-int is_builtin(char **args, char *line)
+int is_builtin(char **args, char *line, int status)
 {
     if (args == NULL || args[0] == NULL)
         return (0);
 
     if (strcmp(args[0], "exit") == 0)
     {
-        handle_exit(line, args);
+        handle_exit(line, args, status);
         return (1);
     }
     else if (strcmp(args[0], "env") == 0)
@@ -56,4 +58,3 @@ int is_builtin(char **args, char *line)
 
     return (0);
 }
-
