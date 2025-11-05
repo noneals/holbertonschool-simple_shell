67a71762f6b1a2da3ee9efc907b48d10a3821a58
root <root@ip-10-42-78-176.ec2.internal>
2025-11-05 00:14:39 +0000

    fix: betty-style builtin.c, find_path.c

--- builtin.c
+++ builtin.c
@@ -1,60 +1,60 @@
 #include "main.h"
 
 /**
- * handle_exit - Handles the exit built-in command
- * @line: The input line to free
- * @args: The argument array to free
- * @status: The last exit status to return
- *
- * Return: void (exits the program)
- */
+	* handle_exit - Handles the exit built-in command
+	* @line: The input line to free
+	* @args: The argument array to free
+	* @status: The last exit status to return
+	*
+	* Return: void (exits the program)
+	*/
 void handle_exit(char *line, char **args, int status)
 {
-    free(line);
-    free_array(args);
-    exit(status);
+	free(line);
+	free_array(args);
+	exit(status);
 }
 
 /**
- * handle_env - Handles the env built-in command
- *
- * Return: 0 on success
- */
+	* handle_env - Handles the env built-in command
+	*
+	* Return: 0 on success
+	*/
 int handle_env(void)
 {
-    char **env = environ;
+	char **env = environ;
 
-    while (*env)
-    {
-        printf("%s\n", *env);
-        env++;
-    }
-    return (0);
+	while (*env)
+	{
+	printf("%s\n", *env);
+	env++;
+	}
+	return (0);
 }
 
 /**
- * is_builtin - Checks if command is a built-in command
- * @args: Array of command arguments
- * @line: The input line (for memory cleanup on exit)
- * @status: The last exit status
- *
- * Return: 1 if built-in executed, 0 otherwise
- */
+	* is_builtin - Checks if command is a built-in command
+	* @args: Array of command arguments
+	* @line: The input line (for memory cleanup on exit)
+	* @status: The last exit status
+	*
+	* Return: 1 if built-in executed, 0 otherwise
+	*/
 int is_builtin(char **args, char *line, int status)
 {
-    if (args == NULL || args[0] == NULL)
-        return (0);
+	if (args == NULL || args[0] == NULL)
+	return (0);
 
-    if (strcmp(args[0], "exit") == 0)
-    {
-        handle_exit(line, args, status);
-        return (1);
-    }
-    else if (strcmp(args[0], "env") == 0)
-    {
-        handle_env();
-        return (1);
-    }
+	if (strcmp(args[0], "exit") == 0)
+	{
+	handle_exit(line, args, status);
+	return (1);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+	handle_env();
+	return (1);
+	}
 
-    return (0);
+	return (0);
 }
--- find_path.c
+++ find_path.c
@@ -1,101 +1,101 @@
 #include "main.h"
 
 /**
- * get_path_env - Gets the PATH environment variable
- *
- * Return: Pointer to PATH value, or NULL if not found or empty
- */
+	* get_path_env - Gets the PATH environment variable
+	*
+	* Return: Pointer to PATH value, or NULL if not found or empty
+	*/
 char *get_path_env(void)
 {
-    int i = 0;
-    char *path_value;
+	int i = 0;
+	char *path_value;
 
-    while (environ[i])
-    {
-        if (strncmp(environ[i], "PATH=", 5) == 0)
-        {
-            path_value = environ[i] + 5;
-            if (*path_value == '\0')
-                return (NULL);
-            return (path_value);
-        }
-        i++;
-    }
-    return (NULL);
+	while (environ[i])
+	{
+	if (strncmp(environ[i], "PATH=", 5) == 0)
+	{
+	path_value = environ[i] + 5;
+	if (*path_value == '\0')
+	return (NULL);
+	return (path_value);
+	}
+	i++;
+	}
+	return (NULL);
 }
 
 /**
- * check_absolute_path - Checks if command is absolute or relative path
- * @command: The command to check
- *
- * Return: Duplicated command path if valid, NULL otherwise
- */
+	* check_absolute_path - Checks if command is absolute or relative path
+	* @command: The command to check
+	*
+	* Return: Duplicated command path if valid, NULL otherwise
+	*/
 char *check_absolute_path(char *command)
 {
-    struct stat st;
+	struct stat st;
 
-    if (command[0] == '/' || command[0] == '.')
-    {
-        if (stat(command, &st) == 0)
-            return (strdup(command));
-    }
-    return (NULL);
+	if (command[0] == '/' || command[0] == '.')
+	{
+	if (stat(command, &st) == 0)
+	return (strdup(command));
+	}
+	return (NULL);
 }
 
 /**
- * search_in_path - Searches for command in PATH directories
- * @command: The command to find
- * @path_env: The PATH environment variable value
- *
- * Return: Full path if found, NULL otherwise
- */
+	* search_in_path - Searches for command in PATH directories
+	* @command: The command to find
+	* @path_env: The PATH environment variable value
+	*
+	* Return: Full path if found, NULL otherwise
+	*/
 char *search_in_path(char *command, char *path_env)
 {
-    char *path_copy, *dir, *full_path;
-    struct stat st;
+	char *path_copy, *dir, *full_path;
+	struct stat st;
 
-    path_copy = strdup(path_env);
-    if (!path_copy)
-        return (NULL);
-    dir = strtok(path_copy, ":");
-    while (dir)
-    {
-        full_path = malloc(strlen(dir) + strlen(command) + 2);
-        if (!full_path)
-        {
-            free(path_copy);
-            return (NULL);
-        }
-        sprintf(full_path, "%s/%s", dir, command);
-        if (stat(full_path, &st) == 0)
-        {
-            free(path_copy);
-            return (full_path);
-        }
-        free(full_path);
-        dir = strtok(NULL, ":");
-    }
-    free(path_copy);
-    return (NULL);
+	path_copy = strdup(path_env);
+	if (!path_copy)
+	return (NULL);
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+	full_path = malloc(strlen(dir) + strlen(command) + 2);
+	if (!full_path)
+	{
+	free(path_copy);
+	return (NULL);
+	}
+	sprintf(full_path, "%s/%s", dir, command);
+	if (stat(full_path, &st) == 0)
+	{
+	free(path_copy);
+	return (full_path);
+	}
+	free(full_path);
+	dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
 }
 
 /**
- * find_path - Finds the full path of a command
- * @command: The command name to search for
- *
- * Return: Full path to command if found, NULL otherwise
- */
+	* find_path - Finds the full path of a command
+	* @command: The command name to search for
+	*
+	* Return: Full path to command if found, NULL otherwise
+	*/
 char *find_path(char *command)
 {
-    char *path_env, *result;
+	char *path_env, *result;
 
-    result = check_absolute_path(command);
-    if (result)
-        return (result);
+	result = check_absolute_path(command);
+	if (result)
+	return (result);
 
-    path_env = get_path_env();
-    if (!path_env)
-        return (NULL);
+	path_env = get_path_env();
+	if (!path_env)
+	return (NULL);
 
-    return (search_in_path(command, path_env));
+	return (search_in_path(command, path_env));
 }
