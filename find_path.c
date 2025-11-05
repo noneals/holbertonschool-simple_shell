e899e138df6b21144cbb571cc30e1cddb1a69dd3
aziz5g-tech <bogico.sa@gmail.com>
2025-11-05 03:00:45 +0300

    fix: solve more 40 lines per functions

--- find_path.c
+++ find_path.c
@@ -13,7 +13,7 @@ char *get_path_env(void)
     while (environ[i])
     {
         if (strncmp(environ[i], "PATH=", 5) == 0)
-            {
+        {
             path_value = environ[i] + 5;
             if (*path_value == '\0')
                 return (NULL);
@@ -25,33 +25,39 @@ char *get_path_env(void)
 }
 
 /**
- * find_path - Finds the full path of a command
- * @command: The command name to search for
+ * check_absolute_path - Checks if command is absolute or relative path
+ * @command: The command to check
  *
- * Return: Full path to command if found, NULL otherwise
+ * Return: Duplicated command path if valid, NULL otherwise
  */
-
-char *find_path(char *command)
+char *check_absolute_path(char *command)
 {
-    char *path_env, *path_copy, *dir, *full_path;
     struct stat st;
 
     if (command[0] == '/' || command[0] == '.')
     {
         if (stat(command, &st) == 0)
             return (strdup(command));
-        return (NULL);
     }
+    return (NULL);
+}
 
-    path_env = get_path_env();
-    if (!path_env)
-        return (NULL);
+/**
+ * search_in_path - Searches for command in PATH directories
+ * @command: The command to find
+ * @path_env: The PATH environment variable value
+ *
+ * Return: Full path if found, NULL otherwise
+ */
+char *search_in_path(char *command, char *path_env)
+{
+    char *path_copy, *dir, *full_path;
+    struct stat st;
 
     path_copy = strdup(path_env);
     if (!path_copy)
         return (NULL);
     dir = strtok(path_copy, ":");
-
     while (dir)
     {
         full_path = malloc(strlen(dir) + strlen(command) + 2);
@@ -69,7 +75,27 @@ char *find_path(char *command)
         free(full_path);
         dir = strtok(NULL, ":");
     }
-
     free(path_copy);
     return (NULL);
+}
+
+/**
+ * find_path - Finds the full path of a command
+ * @command: The command name to search for
+ *
+ * Return: Full path to command if found, NULL otherwise
+ */
+char *find_path(char *command)
+{
+    char *path_env, *result;
+
+    result = check_absolute_path(command);
+    if (result)
+        return (result);
+
+    path_env = get_path_env();
+    if (!path_env)
+        return (NULL);
+
+    return (search_in_path(command, path_env));
 }
